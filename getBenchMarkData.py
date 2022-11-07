9137cd5034c4d6ec9566309c0e85e773ede73c0f
1155141394 <1155141394@link.cuhk.edu.hk>
2022-11-07 22:12:38 +0800

    'bug'

--- getBenchMarkData.py
+++ getBenchMarkData.py
@@ -14,7 +14,7 @@ import numpy as np
 # end_date_iso = end_date.isoformat()
 
 
-os.system('tsbs_generate_data --use-case="iot" --seed=123 --scale=800 '
+os.system('tsbs_generate_data --use-case="iot" --seed=123 --scale=100 '
           '--timestamp-start="2022-10-01T00:00:00Z"'
           ' --timestamp-end="2022-10-05T00:00:00Z"'
           ' --log-interval="10s" --format="timescaledb" '
@@ -30,11 +30,13 @@ conn = psycopg2.connect(host="localhost", port=5432, user="postgres", password="
 cur = conn.cursor()
 # today = date.today()
 beg_date = datetime.strptime("2022-10-01 00:00:00", '%Y-%m-%d %H:%M:%S')
-for i in range(4):
+for i in range(3):
+    beg_time = time.time()
     end_date = beg_date + timedelta(days=1)
 
-    file_name_1 = "benchmark_diagnostics_" + str(beg_date) + ".csv"
-    file_name_2 = "benchmark_readings_" + str(beg_date) + ".csv"
+    file_name_1 = "diagnostics_" + str(beg_date) + ".csv"
+    file_name_2 = "readings_" + str(beg_date) + ".csv"
+    file_name_3 = "tags_" + str(beg_date) + ".csv"
 
     # get data from diagnostics
     sql_get_chunks = r"SELECT show_chunks('diagnostics', older_than => DATE '%s');" % (end_date)
@@ -67,7 +69,7 @@ for i in range(4):
     os.system("aws s3 cp ../%s s3://csfyp2023/benchmark/%s" % (file_name_1, file_name_1))
     os.system("rm -rf ../%s" % (file_name_1))
 
-    # select data from readings
+    # get data from readings
     sql_get_chunks = r"SELECT show_chunks('readings', older_than => DATE '%s');" % (end_date)
     cur.execute(sql_get_chunks)
     data = cur.fetchall()
@@ -98,6 +100,39 @@ for i in range(4):
     os.system("aws s3 cp ../%s s3://csfyp2023/benchmark/%s" % (file_name_2, file_name_2))
     os.system("rm -rf ../%s" % (file_name_2))
 
+    # get data from tags
+    sql_get_chunks = r"SELECT show_chunks('tags', older_than => DATE '%s');" % (end_date)
+    cur.execute(sql_get_chunks)
+    data = cur.fetchall()
+    print(data)
+    latest_chunk = data[0][0]
+
+    sql_get_data = r"copy %s to" \
+                   " '/var/lib/postgresql/%s' delimiter as ',' null as '' escape as '\"' CSV quote as '\"'" % (
+                       latest_chunk, file_name_3)
+    print("File name is %s" % (file_name_3))
+    cur.execute(sql_get_data)
+
+    sql_get_fields = r"select column_name from information_schema.columns where table_schema='public' and table_name='tags';"
+    cur.execute(sql_get_fields)
+    fields = cur.fetchall()
+    print(fields)
+    file_fields = []
+    for field in fields:
+        file_fields.append(field[0])
+
+    csv = pd.read_csv(r'/var/lib/postgresql/%s' % file_name_3, header=None, names=file_fields)
+    csv.to_csv('/var/lib/postgresql/%s' % file_name_3, index=False)
+
+    sql_delete_chunk = r"SELECT drop_chunks('tags', older_than => DATE '%s');" % end_date
+    cur.execute(sql_delete_chunk)
+    # print(cur.fetchall())
+    conn.commit()
+    os.system("aws s3 cp ../%s s3://csfyp2023/benchmark/%s" % (file_name_3, file_name_3))
+    os.system("rm -rf ../%s" % (file_name_3))
+
+    end_time = time.time()
+    print("Time cost for one day data: %f" % (end_time-beg_time))
     beg_date = beg_date + timedelta(days=1)
 conn.close()
 
