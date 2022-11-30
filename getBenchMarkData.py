5a047f57a520446679f33d1ae90f83a1fde4f288
TaoXU0 <464612404@qq.com>
2022-11-30 12:58:29 +0800

    注释掉tags部分

--- getBenchMarkData.py
+++ getBenchMarkData.py
@@ -14,9 +14,9 @@ import numpy as np
 # end_date_iso = end_date.isoformat()
 
 
-os.system('tsbs_generate_data --use-case="iot" --seed=123 --scale=200 '
-          '--timestamp-start="2022-10-03T00:00:00Z"'
-          ' --timestamp-end="2022-10-04T00:00:00Z"'
+os.system('tsbs_generate_data --use-case="iot" --seed=123 --scale=100 '
+          '--timestamp-start="2022-10-01T00:00:00Z"'
+          ' --timestamp-end="2022-10-02T00:00:00Z"'
           ' --log-interval="10s" --format="timescaledb" '
           '| gzip > ./timescaledb-data.gz')
 
