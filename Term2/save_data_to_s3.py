954796c3a1980542657cbc9102390dcdd1d4c577
xutao <464612404@qq.com>
2023-02-19 19:19:41 +0800

    Change file name format

--- Term2/save_data_to_s3.py
+++ Term2/save_data_to_s3.py
@@ -10,7 +10,6 @@ def save_data_to_s3(bucket, tsid, time_start, time_end, data_path):
     time_start_str = time_start.strftime("%Y-%m-%d:%H")
     time_end_str = time_end.strftime("%Y-%m-%d:%H")
     file_name = "%s-%s" % (time_start_str, time_end_str)
-    file_name = "%d-%d" % (time_start, time_end)
     os.system("aws s3 cp %s s3://%s/%d/%s" % (data_path, bucket, tsid, file_name))
     print("Save the file to S3 successfully.")
 
