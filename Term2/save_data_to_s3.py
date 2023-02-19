93728b9b593c659e7ccc198b774bbd9fb6e4dc46
xutao <464612404@qq.com>
2023-02-19 19:35:48 +0800

    Copy time_index

--- Term2/save_data_to_s3.py
+++ Term2/save_data_to_s3.py
@@ -4,7 +4,28 @@ from datetime import date
 import datetime
 import pandas as pd
 import numpy as np
-from cpu_retrieve import time_index
+def time_index(start_t, end_t):
+    hours=[]
+    if start_t == None:
+        end_h = end_t.hour
+        end_index = end_t//2 + 1
+        for i in range(1,end_index+1):
+                hours.append(i)
+        return hours
+    elif end_t == None:
+        start_h = start_t.hour
+        start_index = start_h//2 + 1
+        for i in range(start_index,13):
+            hours.append(i)
+        return hours
+    else:
+        start_h = start_t.hour
+        end_h = end_t.hour
+        start_index = start_h//2 + 1
+        end_index = end_t//2 + 1
+        for i in range(start_index,end_index+1):
+            hours.append(i)
+        return hours
 
 
 def save_data_to_s3(bucket, tsid, time_start, time_end, data_path):
