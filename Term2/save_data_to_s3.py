1aa5a9b488794fcdc8e5a61cfd2b8a6d0af3cd1d
xutao <464612404@qq.com>
2023-02-19 19:18:35 +0800

    update the if sentence

--- Term2/save_data_to_s3.py
+++ Term2/save_data_to_s3.py
@@ -5,12 +5,11 @@ import datetime
 import pandas as pd
 import numpy as np
 
-t = datetime.datetime(1970, 1, 1)
-
-
 def save_data_to_s3(bucket, tsid, time_start, time_end, data_path):
-    time_start = int((time_start-t).total_seconds())
-    time_end = int((time_end-t).total_seconds())
+
+    time_start_str = time_start.strftime("%Y-%m-%d:%H")
+    time_end_str = time_end.strftime("%Y-%m-%d:%H")
+    file_name = "%s-%s" % (time_start_str, time_end_str)
     file_name = "%d-%d" % (time_start, time_end)
     os.system("aws s3 cp %s s3://%s/%d/%s" % (data_path, bucket, tsid, file_name))
     print("Save the file to S3 successfully.")
