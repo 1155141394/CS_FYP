fda3b19d509a5c8cab3eb846731c8b505a2c9542
xutao <464612404@qq.com>
2023-02-19 19:21:41 +0800

    Change file name format

--- Term2/save_data_to_s3.py
+++ Term2/save_data_to_s3.py
@@ -6,10 +6,10 @@ import pandas as pd
 import numpy as np
 
 def save_data_to_s3(bucket, tsid, time_start, time_end, data_path):
-
-    time_start_str = time_start.strftime("%Y-%m-%d:%H")
-    time_end_str = time_end.strftime("%Y-%m-%d:%H")
+    generated_date = time_start.strftime("%Y-%m-%d")
+    time_start_str = time_start.strftime("%H")
+    time_end_str = time_end.strftime("%H")
     file_name = "%s-%s" % (time_start_str, time_end_str)
-    os.system("aws s3 cp %s s3://%s/%d/%s" % (data_path, bucket, tsid, file_name))
+    os.system("aws s3 cp %s s3://%s/%d/%s/%s" % (data_path, bucket, tsid, generated_date, file_name))
     print("Save the file to S3 successfully.")
 
