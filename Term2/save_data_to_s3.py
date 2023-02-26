53f56ec6ca73f193e926fec9f51d727ee0eb8327
xutao <464612404@qq.com>
2023-02-26 17:32:16 +0800

    Update data storage in S3

--- Term2/save_data_to_s3.py
+++ Term2/save_data_to_s3.py
@@ -31,6 +31,6 @@ def time_index(start_t, end_t):
 def save_data_to_s3(bucket, tsid, time_start, time_end, data_path):
     generated_date = time_start.strftime("%Y-%m-%d")
     index = time_index(time_start, time_end)[0]
-    file_name = "%d.csv" % index
-    os.system("aws s3 cp %s s3://%s/%d/%s/%s" % (data_path, bucket, tsid, generated_date, file_name))
+    file_name = f"{tsid}_{generated_date}_{index}.csv"
+    os.system("aws s3 cp %s s3://%s/%s" % (data_path, bucket, file_name))
     print("Save the file to S3 successfully.")
