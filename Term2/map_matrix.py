c7cdf214c1f15f91bd43ed8c173271bc6d6b21fb
taozhisheng <1155141394@link.cuhk.edu.hk>
2023-03-12 17:11:07 +0800

    add set&file

--- Term2/map_matrix.py
+++ Term2/map_matrix.py
@@ -2,6 +2,66 @@ import psycopg2
 import pandas as pd
 from tools import *
 from hash import HashTable
+def data_mapping(tags_name,value_name,des,lines):
+    attr = []
+    for item in des:
+        attr.append(item[0])
+    map_matrix = []
+    tags_pair_set = set()
+
+    for line in lines:
+        tags_value = []
+        value = []
+        index_list = []
+        tags_str = ''
+        for i in range(len(tags_name)):
+            # 获得表中attr中的值
+            tags_value.append(line[attr.index(tags_name[i])])
+
+        for i in range(len(value_name)):
+            value.append(line[attr.index(value_name[i])])
+
+        for tag in tags_value:
+            # 获得cpu和node对应的hash值
+            index_list.append(index(index_map, tag))
+            tags_str += tag + '_'
+
+        is_exist = 1 if tags_str in tags_pair_set else 0
+        if is_exist:
+            tsid = 0
+            for i in range(len(map_matrix)):
+                flag = 1
+                for indexes in index_list:
+                    if map_matrix[i][indexes] != 1:
+                        flag = 0
+                        break
+                if flag:
+                    tsid = i
+                    break
+            insert(tsid, value, value_name)
+            continue
+        else:
+            tags_pair_set.add(tags_str)
+            new_TS = [0] * 5000
+            tsid = len(map_matrix)
+            map_matrix.append(new_TS)
+            for indexes in index_list:
+                map_matrix[tsid][indexes] = 1
+
+            insert(tsid, value, value_name)
+
+    write_set_to_file(tags_pair_set, '/home/postgres/CS_FYP/data/query_set.txt')
+    index_map.save_hash('/home/postgres/CS_FYP/data/query_hash')
+    # 提交数据
+    conn.commit()
+    # 关闭连接
+    conn.close()
+    compress_arr, shape = compress_array(map_matrix)
+    print(compress_arr, shape)
+    map_matrix = decompress_array(compress_arr, shape)
+    df = pd.DataFrame(map_matrix)
+    df.to_csv('/home/postgres/CS_FYP/data/map_matrix.csv', index=False, header=False)
+
 
 conn = psycopg2.connect(
     database="example", user="postgres", password="1234", host="localhost", port="5432"
@@ -16,48 +76,12 @@ index_map = HashTable(length=5000)
 # Fetching 1st row from the table
 lines = cursor.fetchall()
 des = cursor.description
-attr = []
-for item in des:
-    attr.append(item[0])
-map_matrix = []
-cpu_node = set()
-for line in lines:
-    # 获得表中attr中的值
-    node = line[attr.index("node")]
-    cpu = line[attr.index("cpu")]
-    time = line[attr.index("time")]
-    cpu_usage = line[attr.index("cpu_usage")]
-    print(node)
-    # 获得cpu和node对应的hash值
-    node_index = index(index_map, node)
-    cpu_index = index(index_map, cpu)
-    is_exist = 1 if cpu + '_' + node in cpu_node else 0
-    if is_exist:
-        tsid = 0
-        for i in range(len(map_matrix)):
-            if map_matrix[i][node_index] == 1 and map_matrix[i][cpu_index] == 1:
-                tsid = i
-                break
-        insert(tsid, time, cpu_usage, ["time", "value"])
-        continue
-    else:
-        cpu_node.add(node + '_' + cpu)
-        new_TS = [0] * 5000
-        tsid = len(map_matrix)
-        map_matrix.append(new_TS)
-        map_matrix[tsid][cpu_index] = 1
-        map_matrix[tsid][node_index] = 1
-        insert(tsid, time, cpu_usage, ["time", "value"])
+data_mapping()
+tags_name =  ["tags_id", "hostname"]
+value_name = ['usage_user','usage_system' ,'usage\
+_idle' ,'usage_nice','usage_iowait','usage_irq' ,'usage_softirq','usage_steal','us\
+age_guest','usage_guest_nice','additional_tags']
+data_mapping(tags_name,value_name,des,lines)
 
-write_set_to_file(cpu_node, '/home/postgres/CS_FYP/data/query_set.txt')
-index_map.save_hash('/home/postgres/CS_FYP/data/query_hash')
-# 提交数据
-conn.commit()
-# 关闭连接
-conn.close()
-compress_arr,shape = compress_array(map_matrix)
-print(compress_arr,shape)
-map_matrix = decompress_array(compress_arr,shape)
-df = pd.DataFrame(map_matrix)
-df.to_csv('/home/postgres/CS_FYP/data/map_matrix.csv', index=False, header=False)
+
 
