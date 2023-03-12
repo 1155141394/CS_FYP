c1ebb5262affef62af58e19d6cee764b728e5e26
xutao <464612404@qq.com>
2023-03-12 19:50:26 +0800

    update insert

--- Term2/map_matrix.py
+++ Term2/map_matrix.py
@@ -72,7 +72,7 @@ conn.autocommit = True
 # 使用cursor()方法创建游标对象
 cursor = conn.cursor()
 # 检索数据
-cursor.execute('''SELECT * from cpu limit 5000''')
+cursor.execute('''SELECT * from cpu''')
 index_map = HashTable(length=5000)
 # Fetching 1st row from the table
 lines = cursor.fetchall()
