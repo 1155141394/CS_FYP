f0001283eb11ad084d6bc2022602328fffa69ea3
taozhisheng <1155141394@link.cuhk.edu.hk>
2023-03-19 20:09:57 +0800

    add set&file

--- Term2/tools.py
+++ Term2/tools.py
@@ -32,7 +32,7 @@ def decompress_array(compressed_arr):
     """将一个压缩后的数组解压缩为原始的二维数组"""
     rows = compressed_arr[len(compressed_arr)-2] # 获取原始数组的行列数
     cols = compressed_arr[len(compressed_arr)-1]
-    print(rows)
+    print(compressed_arr)
     rows = int(rows)
     cols = int(cols)
     compressed_arr = compressed_arr[:-2]
