de03c1ce525f507480affad2602487cbc8ac8379
TaoXU0 <464612404@qq.com>
2023-03-31 11:42:53 +0800

    Update s3_select

--- Term2/query.cpp
+++ Term2/query.cpp
@@ -139,7 +139,7 @@ std::vector<std::string> s3_select(std::string bucket_name, std::string object_k
         isRecordsEventReceived = true;
         auto recordsVector = recordsEvent.GetPayload();
         Aws::String records(recordsVector.begin(), recordsVector.end());
-        std::string s(aws_s.c_str(), aws_s.size());
+        std::string s(records.c_str(), records.size());
         s3_result = s;
         ASSERT_STREQ(firstColumn.c_str(), records.c_str());
     });
