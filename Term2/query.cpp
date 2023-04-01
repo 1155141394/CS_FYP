3c2070ba9e03731d817abe65f647c876469ff871
xutao <464612404@qq.com>
2023-04-01 22:12:49 +0800

    update

--- Term2/query.cpp
+++ Term2/query.cpp
@@ -100,7 +100,7 @@ vector<int> find_rows(std::vector<std::vector<int>> arr, int index1, int index2)
 
 
 
-int s3_select(std::string * s3_result, std::string bucket_name, std::string object_key, std::string expression)
+int s3_select(std::string s3_result, std::string bucket_name, std::string object_key, std::string expression)
 {
     Aws::SDKOptions options;
 //        request.SetResponseStreamFactory([] { return new std::fstream("jianming.csv", std::ios_base::out); });
