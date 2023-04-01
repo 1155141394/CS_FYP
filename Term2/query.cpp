43e6cbe47e4c7a0699043633f653a1282e0596c0
xutao <464612404@qq.com>
2023-04-01 22:33:48 +0800

    update

--- Term2/query.cpp
+++ Term2/query.cpp
@@ -96,7 +96,31 @@ vector<int> find_rows(std::vector<std::vector<int>> arr, int index1, int index2)
 }
 
 
+//现代写法完成String深拷贝
+String::String(const char* str)
+    : _str(new char[strlen(str) + 1])
+{
+    strcpy(_str, str);
+}
 
+String::String(const String& s)
+{
+    String tmp(s._str); // 用s._str定义一个tmp对象
+    swap(_str, tmp._str); // 将指针进行交换
+}
+
+String& String::operator=(String s) // 形参调用构造函数
+{
+    swap(_str, s._str);
+    return *this;
+}
+
+String::~String()
+{
+    if (NULL != _str)
+    {
+        delete[] _str;
+    }
 
 
 
@@ -155,7 +179,7 @@ std::string s3_select(std::string bucket_name, std::string object_key, std::stri
 //        cout << "Get string successfully." << endl;
 //        return records.c_str();
         std::string s(records.c_str(), records.size());
-        strcpy(s3_result, s.c_str());
+        s3_result = s.c_str();
 //        ASSERT_STREQ(firstColumn.c_str(), records.c_str());
     });
     cout << "SetRecordsEventCallback" << endl;
