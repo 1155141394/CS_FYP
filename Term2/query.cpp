175e284c473d16e3910479192a191498c02cb8b9
xutao <464612404@qq.com>
2023-04-01 22:12:22 +0800

    update

--- Term2/query.cpp
+++ Term2/query.cpp
@@ -100,7 +100,7 @@ vector<int> find_rows(std::vector<std::vector<int>> arr, int index1, int index2)
 
 
 
-int s3_select(std::string s3_result, std::string bucket_name, std::string object_key, std::string expression)
+int s3_select(std::string * s3_result, std::string bucket_name, std::string object_key, std::string expression)
 {
     Aws::SDKOptions options;
 //        request.SetResponseStreamFactory([] { return new std::fstream("jianming.csv", std::ios_base::out); });
@@ -250,7 +250,7 @@ int main()
     for (int i = 0; i < vec.size(); i++)
     std::cout << vec[i] << ' ';
     std::string s3_result;
-    s3_select(s3_result, "fypts", "0/2023-01-01_12.csv", "SELECT * FROM s3object limit 5");
+    s3_select(&s3_result, "fypts", "0/2023-01-01_12.csv", "SELECT * FROM s3object limit 5");
     cout << s3_result <<endl;
 
 
