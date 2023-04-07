4acfa55d3c8d6473ebc52ab58ef3b28793b59b02
xutao <464612404@qq.com>
2023-04-07 16:04:09 +0800

    add attr type

--- Others/reset.py
+++ Others/reset.py
@@ -29,7 +29,7 @@ import numpy as np
 conn = psycopg2.connect(host="localhost", port=5432, user="postgres", password="1234", database="benchmark")
 cur = conn.cursor()
 # today = date.today()
-beg_date = datetime.strptime("2023-04-03 00:00:00", '%Y-%m-%d %H:%M:%S')
+beg_date = datetime.strptime("2023-04-02 00:00:00", '%Y-%m-%d %H:%M:%S')
 for i in range(6):
     beg_time = time.time()
     end_date = beg_date + timedelta(days=1)
