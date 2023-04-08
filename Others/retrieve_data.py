11b28b99dd33b3790df3c3ab9268c4b45b2323c0
xutao <464612404@qq.com>
2023-04-08 16:13:50 +0800

    add attr type

--- Others/retrieve_data.py
+++ Others/retrieve_data.py
@@ -108,8 +108,8 @@ if __name__ == "__main__":
 
     # Let user input command
     table_name = 'cpu'
-    start_time = '2023-04-07 12:26:46'
-    end_time = '2023-04-07 13:26:46'
+    start_time = '2023-04-06 23:00:44'
+    end_time = '2023-04-07 07:00:44'
 
     sql_select = "select * from %s where time > '%s' and time < '%s';"%(table_name, start_time, end_time)
 
@@ -173,6 +173,12 @@ if __name__ == "__main__":
         FROM cpu
         WHERE tags_id IN (SELECT id FROM tags WHERE hostname IN ('host_9','host_43','host_75','host_19','host_39','host_35','host_15','host_41')) AND time >= '2023-04-07 12:26:46.646325 +0000' AND time < '2023-04-07 13:26:46.646325 +0000'
         GROUP BY minute ORDER BY minute'''
+
+    query_max_all_1 = '''SELECT time_bucket('3600 seconds', time) AS hour,
+        max(usage_user) as max_usage_user, max(usage_system) as max_usage_system, max(usage_idle) as max_usage_idle, max(usage_nice) as max_usage_nice, max(usage_iowait) as max_usage_iowait, max(usage_irq) as max_usage_irq, max(usage_softirq) as max_usage_softirq, max(usage_steal) as max_usage_steal, max(usage_guest) as max_usage_guest, max(usage_guest_nice) as max_usage_guest_nice
+        FROM cpu
+        WHERE tags_id IN (SELECT id FROM tags WHERE hostname IN ('host_75')) AND time >= '2023-04-06 23:00:44.894865 +0000' AND time < '2023-04-07 07:00:44.894865 +0000'
+        GROUP BY hour ORDER BY hour'''
     cur.execute(query_581)
 
     conn.commit()
