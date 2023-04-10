abb2fd346a9a4ad1b4bf807392d5a7c7fa733892
xutao <464612404@qq.com>
2023-04-10 15:52:16 +0800

    add attr type

--- Others/retrieve_data.py
+++ Others/retrieve_data.py
@@ -108,8 +108,8 @@ if __name__ == "__main__":
 
     # Let user input command
     table_name = 'cpu'
-    start_time = '2023-04-06 11:35:31'
-    end_time = '2023-04-07 11:35:31'
+    start_time = '2023-04-09 09:22:40'
+    end_time = '2023-04-09 10:22:40'
 
     sql_select = "select * from %s where time > '%s' and time < '%s';"%(table_name, start_time, end_time)
 
@@ -171,7 +171,7 @@ if __name__ == "__main__":
     query_581 = '''SELECT time_bucket('60 seconds', time) AS minute,
         max(usage_user) as max_usage_user, max(usage_system) as max_usage_system, max(usage_idle) as max_usage_idle, max(usage_nice) as max_usage_nice, max(usage_iowait) as max_usage_iowait
         FROM cpu
-        WHERE tags_id IN (SELECT id FROM tags WHERE hostname IN ('host_9','host_43','host_75','host_19','host_39','host_35','host_15','host_41')) AND time >= '2023-04-07 12:26:46.646325 +0000' AND time < '2023-04-07 13:26:46.646325 +0000'
+        WHERE tags_id IN (SELECT id FROM tags WHERE hostname IN ('host_9','host_43','host_75','host_19','host_39','host_35','host_15','host_41')) AND time >= '2023-04-09 09:22:40.646325 +0000' AND time < '2023-04-09 10:22:40.646325 +0000'
         GROUP BY minute ORDER BY minute'''
 
     query_max_all_1 = '''SELECT time_bucket('3600 seconds', time) AS hour,
@@ -180,11 +180,13 @@ if __name__ == "__main__":
         WHERE tags_id IN (SELECT id FROM tags WHERE hostname IN ('host_1')) AND time >= '2023-04-06 23:00:44.894865 +0000' AND time < '2023-04-07 07:00:44.894865 +0000'
         GROUP BY hour ORDER BY hour'''
 
+    high_cpu_12 = '''SELECT * FROM cpu WHERE usage_user > 90.0 and time >= '2023-04-08 23:35:31.138978 +0000' AND time < '2023-04-09 11:35:31.138978 +0000' AND tags_id IN (SELECT id FROM tags WHERE hostname IN ('host_75'))'''
+
     lastpoint = '''SELECT DISTINCT ON (t.hostname) * FROM tags t INNER JOIN LATERAL(SELECT * FROM cpu c WHERE c.tags_id = t.id ORDER BY time DESC LIMIT 1) AS b ON true ORDER BY t.hostname, b.time DESC'''
 
-    query_high_cpu_1 = '''SELECT * FROM cpu WHERE usage_user > 80.0 and time >= '2023-04-06 11:35:31.138978 +0000' AND time < '2023-04-07 11:35:31.138978 +0000' AND tags_id = 76;'''
+    query_high_cpu_1 = '''SELECT usage_user FROM cpu WHERE usage_user > 80.0 and time >= '2023-04-06 11:35:31.138978 +0000' AND time < '2023-04-07 11:35:31.138978 +0000' AND tags_id = 76;'''
 
-    cur.execute(lastpoint)
+    cur.execute(query_581)
 
     conn.commit()
     print(cur.fetchall())
