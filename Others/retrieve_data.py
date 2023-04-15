bee8b9485ec5a305c1defce4f5947b2aad8b5054
xutao <464612404@qq.com>
2023-04-15 21:16:54 +0800

    add attr type

--- Others/retrieve_data.py
+++ Others/retrieve_data.py
@@ -109,8 +109,8 @@ if __name__ == "__main__":
 
     # Let user input command
     table_name = 'cpu'
-    start_time = '2023-04-10 13:22:40'
-    end_time = '2023-04-10 21:22:40'
+    start_time = '2023-04-10 14:22:40'
+    end_time = '2023-04-10 22:22:40'
 
     cur = conn.cursor()
 
