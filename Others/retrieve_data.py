53244893c101057d5ebfe09591c0e315e36c3807
taozhisheng <1155141394@link.cuhk.edu.hk>
2023-04-15 10:24:58 +0800

    update

--- Others/retrieve_data.py
+++ Others/retrieve_data.py
@@ -94,9 +94,9 @@ def s3_select(table_name, beg_t, end_t):
                 print("Stats details bytesReturned: ")
                 print(statsDetails['BytesReturned'])
         for line in (com_rec.splitlines()):
-            #print(line)
+            print(line)
             data.append(line.split(","))
-    print(data)
+    # print(data)
     df = pd.DataFrame(data)
     file_name = 'tmp.csv'
     df.to_csv('/var/lib/postgresql/'+file_name, index=False, header=False)
